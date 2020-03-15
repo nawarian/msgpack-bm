a2cdaba83b25dfa1dd9c75f271115869cd0e2a45
Nawarian <nickolas@phpsp.org.br>
2020-03-15 12:18:26 +0100

    Add travis set up

---
+++ bin/benchmark-json.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'Run json benchmark' . PHP_EOL;
+
+echo 'Json exists: ' . (function_exists('json_encode') ? 'yes' : 'no') . PHP_EOL;
---
+++ bin/benchmark-msgpack.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'Run msgpack benchmark' . PHP_EOL;
+
+echo 'MsgPack exists: ' . (function_exists('msgpack_pack') ? 'yes' : 'no') . PHP_EOL;
